54dbcd43dd2998c0e92f2060fdbefd1abd851f64
fujibanko <so.fujibank@gmail.com>
2023-06-21 16:37:46 +0800

    fix: fix css safelist hover not working

--- lib/util/cssUtil.ts
+++ lib/util/cssUtil.ts
@@ -3,9 +3,9 @@ import type { CustomFlowbiteTheme } from 'flowbite-react';
 export const customTheme: CustomFlowbiteTheme = {
     button: {
       color: {
-        navy: 'bg-navy hover:bg-navy/[0.9] text-lightblue',
-        white: 'bg-white hover:bg-white/[0.9] border border-navy text-navy',
-        outline: 'w-full bg-white hover:bg-white/[0.9] border border-navy text-navy',
+        navy: 'bg-navy hover:bg-navy/90 text-lightblue',
+        white: 'bg-white hover:bg-white/90 border border-navy text-navy dark:text-black',
+        outline: 'w-full bg-white border border-navy text-navy',
       },
     },
   };
--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,12 @@ module.exports = {
   safelist: [
     {
       pattern:
-        /(bg|text|border)-(navy|lightblue|pink|blue|gray|lightgray|darkgray|white|enabled)/,
+        /(bg|text|border)-(navy|lightblue|pink|blue|gray|lightgray|darkgray|white|black|enabled)/,
+    },
+    {
+      pattern:
+        /bg-(navy|lightblue|pink|blue|gray|lightgray|darkgray|white|black|enabled)\/(5|10|20|30|40|50|60|70|80|90|100)/,
+      variants: ['hover', 'focus'],
     },
   ],
 };
