2fce391bba6fefa5ed39939fac534ac195d0c5a9
fujibanko <so.fujibank@gmail.com>
2023-06-21 17:29:18 +0800

    feat: add OTP base component

---
+++ ui/controls/OTPField.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useRef, useState } from 'react';
+
+export interface OTPFieldProps {}
+
+export function OTPField(props: OTPFieldProps) {
+  useEffect(() => {}, []);
+
+  return <></>;
+}
